9f08377932d32df89d74ebd98a14ad04fe713e64
Vperus <vperus@gmail.com>
2017-11-16 20:59:30 +0200

    Add server struct

--- main.go
+++ main.go
@@ -11,8 +11,18 @@ func check(e error) {
 	}
 }
 
+type server struct {
+	greetingMsg	string
+	allMsg		[]string
+	name		string
+	userNumber	int
+}
+
 func main() {
 	dat, err := ioutil.ReadFile("./program-version")
 	check(err)
 	fmt.Print(string(dat))
+
+	server := server{"Hello", []string{}, "Test-Server", 0}
+	fmt.Print(server.greetingMsg)
 }
