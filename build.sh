27ec516a4beb50cb470fa3f086d37d9c88966e77
V.Kalyuzhny <vperus@gmail.com>
2017-12-12 00:01:06 +0200

    Save version in .go file

--- build.sh
+++ build.sh
@@ -1,17 +1,36 @@
+#!/bin/bash
+
 PROJECT_DIR=$(pwd)
 
+generate_version() {
+        local gitVersion=$(git describe)
+
+        local programVersion=$(cat << VR | sed "s/@@PLACEHOLDER@@/${gitVersion}/g"
+package main
+
+var (
+        VERSION string = "@@PLACEHOLDER@@"
+)
+VR
+)
+        echo "${programVersion}" > cmd/version.go 
+}
+
 build() {
-        if [[ ! -e build ]]; then
+        if [[ ! -d "build" ]]; then
                 mkdir build
         else
                 rm -rf build/irc-client
         fi
 
-        cd cmd
+        generate_version
+
+        cd "cmd"
         go build -o ${PROJECT_DIR}/build/irc-client
         if [[ ! -e ${PROJECT_DIR}/misc/UserConfigs.json ]]; then
                 cp ${PROJECT_DIR}/misc/UserConfigs.json ${PROJECT_DIR}/build
         fi
+        cd ${PROJECT_DIR}
 }
 
 build
--- run.sh
+++
@@ -1,3 +0,0 @@
-cd build
-./irc-client
-cd ..
