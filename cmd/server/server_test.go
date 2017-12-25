34e62fd90df05b5f0ebbb911fdb3dab11bead6af
yura <yura_kozel@ukr.net>
2017-12-25 20:32:34 +0200

    Test repair

--- cmd/server/server_test.go
+++ cmd/server/server_test.go
@@ -36,7 +36,7 @@ func TestGetClientFromName(t *testing.T) {
 	assert.Equal(testClient1, mockServer.getClientFromName("test"), "Should be nil")
 
 	var testClient2 = Client{
-		net.Dial("tcp", "golang.org:80")
+		net.Dial("tcp", "golang.org:80"),
 		make(map[*Room]bool),
 		"Mad Client",
 		"Mad Client",
